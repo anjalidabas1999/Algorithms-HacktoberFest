7793a19703245ff702c8608ff1b516153d5e033f
Anjali <49088065+anjalidabas1999@users.noreply.github.com>
2019-10-05 22:08:35 +0530

    Update factorial.py

--- Python/factorial.py
+++ Python/factorial.py
@@ -1,6 +1,10 @@
-ans = 1
-n = int(input())
-
-for i in range(1,n+1):
-  ans *= i
-print(ans)
+#HacktoberFest 
+[Algorithm for factorial of a number]
+Step a:- Declare n and f as integer variable.
+Step b:- Initialize f=1.
+Step c:-  Enter the value of n.
+Step d:- Check whether n>0, if not then f=1.
+Step e:- If yes then, f=f*n
+Step f:- Decrease the value of n by 1 .
+Step g:- Repeat step 4 and 5 until n=0.
+Step h:- Now print the value of f as factorial of number n.
